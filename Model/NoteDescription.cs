abcf872a9ad9a863856bcffe44b4268382dc7ac2
aadennis <dennis.circ@gmail.com>
2016-02-06 11:06:22 +0000

    Docs update
    Docs update

--- Model/NoteDescription.cs
+++ Model/NoteDescription.cs
@@ -5,8 +5,17 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace NotesApp {
+    /// <summary>
+    /// POCO containing metadata about a musical note
+    /// </summary>
     public class NoteDescription {
+        /// <summary>
+        /// The identifier of a note
+        /// </summary>
         public string Note { get; set; }
+        /// <summary>
+        /// The name of the note phrased to remove any doubt about its pronunciation in English
+        /// </summary>
         public string NameAsSpoken { get; set; }
     }
 }
--- Model/NoteDescriptions.cs
+++ Model/NoteDescriptions.cs
@@ -3,7 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 
 namespace NotesApp {
+
+    /// <summary>
+    /// Metadata for the 12 chromatic notes C to B. Currently limited to the spoken English for each
+    /// note, with a usecase of an application which speaks the names of notes aloud.
+    /// </summary>
     public static class NoteDescriptions {
+
+        /// <summary>
+        /// The set of notes C to B
+        /// </summary>
         private static readonly List<NoteDescription> NoteDescriptionSet = new List<NoteDescription> {
             new NoteDescription {Note = "C", NameAsSpoken = "C"},
             new NoteDescription {Note = "C#", NameAsSpoken = "C sharp"},
@@ -19,10 +28,20 @@ namespace NotesApp {
             new NoteDescription {Note = "B", NameAsSpoken = "B"}
         };
 
+        /// <summary>
+        /// Return the full set of notes C to B
+        /// </summary>
+        /// <returns>the set of note descriptions</returns>
         public static List<NoteDescription> GetAllNoteDescriptions() {
             return NoteDescriptionSet;
         }
 
+        /// <summary>
+        /// Given a single note identifier as key, return the details matching that indentifier.
+        /// If the key is note found, an exception is thrown.
+        /// </summary>
+        /// <param name="note">the key of the note to find in the set of notes</param>
+        /// <returns>the details of the found note</returns>
         public static string GetSpokenNameForNote(string note) {
             var found = NoteDescriptionSet.Any(noted => noted.Note.Equals(note));
             if (!found) {
