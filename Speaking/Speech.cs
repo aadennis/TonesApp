b02401f38798f4d1a56c032274c105475c4b2989
aadennis <dennis.circ@gmail.com>
2016-03-06 16:01:02 +0000

    Add SetVoice method
    Add SetVoice method

--- Speaking/Speech.cs
+++ Speaking/Speech.cs
@@ -1,4 +1,5 @@
 ﻿using System.Speech.Synthesis;
+using System.Linq;
 
 namespace Speaking
 {
@@ -8,16 +9,27 @@ namespace Speaking
     /// </summary>
     public class Speech  {
         private readonly SpeechSynthesizer _speechSynthesizer;
+        //private const string PreferredVoice = "IVONA 2 Brian OEM";
+        private const string PreferredVoice = "Microsoft Hazel Desktop";
 
         public Speech() {
             _speechSynthesizer = new SpeechSynthesizer();
-            //var x = _speechSynthesizer.GetInstalledVoices();
-            _speechSynthesizer.SelectVoice("Microsoft David Desktop");
-
+            SetVoice();
         }
 
         public void Speak(string stringToSpeak) {
             _speechSynthesizer.Speak(stringToSpeak);
+        }
+
+        private void SetVoice() {
+            var availableVoices = _speechSynthesizer.GetInstalledVoices();
+            var voice = availableVoices.SingleOrDefault(x => x.VoiceInfo.Name.Equals(PreferredVoice));
+            
+            if (voice != null) {
+                _speechSynthesizer.SelectVoice(PreferredVoice);
+            } else {
+                _speechSynthesizer.SelectVoiceByHints(VoiceGender.Male);
+            }
         }
     }
 
